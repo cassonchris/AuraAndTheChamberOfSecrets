fec1e13f6ca2dd24d99c0f5ba28b717bdbb04b64
Chris Casson <cassonchris723@gmail.com>
2018-04-01 20:42:35 -0500

    Add BaseRepository.

---
+++ AuraAndTheChamberOfSecrets.Repo/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AuraAndTheChamberOfSecrets.Repo.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuraAndTheChamberOfSecrets.Repo
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly AuraAndTheChamberOfSecretsDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(AuraAndTheChamberOfSecretsDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+
+        public T GetSingleById(Guid id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> query)
+        {
+            return _dbSet.Where(query);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
