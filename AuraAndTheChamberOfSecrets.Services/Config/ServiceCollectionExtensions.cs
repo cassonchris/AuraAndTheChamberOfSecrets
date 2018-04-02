7a8219a72e8935af8ba0a21d70e99e221eb915bd
Chris Casson <cassonchris723@gmail.com>
2018-04-02 14:57:22 -0500

    Add Services project.

---
+++ AuraAndTheChamberOfSecrets.Services/Config/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AuraAndTheChamberOfSecrets.Services.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuraAndTheChamberOfSecrets.Services.Config
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAuraAndTheChamberOfSecretsServices(this IServiceCollection services)
+        {
+            services.AddScoped<IQuestionService, QuestionService>();
+        }
+    }
+}
---
+++ AuraAndTheChamberOfSecrets.Services/Interface/IQuestionService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using AuraAndTheChamberOfSecrets.Models;
+
+namespace AuraAndTheChamberOfSecrets.Services.Interface
+{
+    public interface IQuestionService
+    {
+        IQueryable<Question> SearchQuestions(string searchString);
+    }
+}
